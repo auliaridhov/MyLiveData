1a8d1c91c374526b4c8d802b63469f34c3d7fac7
Aulia Ridho Verdikha <auliaridhoverdika@gmail.com>
2020-11-10 20:46:19 +0700

    Initial commit

---
+++ app/src/main/java/com/harvdev/mylivedata/MainActivity.java
@@ -0,0 +1,35 @@
+package com.harvdev.mylivedata;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel mLiveDataTimerViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLiveDataTimerViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        subscribe();
+    }
+
+    private void subscribe() {
+        final Observer<Long> elapsedTimeObserver = new Observer<Long>() {
+            @Override
+            public void onChanged(@Nullable final Long aLong) {
+                String newText = MainActivity.this.getResources().getString(R.string.seconds, aLong);
+                ((TextView) findViewById(R.id.timer_textview)).setText(newText);
+            }
+        };
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver);
+    }
+}
